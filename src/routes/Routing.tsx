a76dc902a8c4d099ca8f6d7bb32d5d7949b35a03
godcl1623 <godcl1623@gmail.com>
2022-08-01 23:25:36 +0900

    test : 달력 구현 테스트 - 오늘 날짜 기준, 달력 구현을 위한 날짜 받아오기 구현 - 예약 범위 기준일 지정, 예약 범위 지정 구현 - 예약 범위에 따른 스타일 변화 구현 - 이번달, 다음달 전환 버튼 구현

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailPage from 'pages/detailPage/DetailPage';
 import MainPage from 'pages/mainPage/MainPage';
 import StatusPage from 'pages/statusPage/StatusPage';
+import Test from 'calender/ChihangCalender/test';
 import Path from './Path';
 
 function Routing() {
@@ -11,7 +12,7 @@ function Routing() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path={Path.status} element={<StatusPage />} />
-        <Route path={Path.detail} element={<DetailPage />} />
+        <Route path={Path.detail} element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
