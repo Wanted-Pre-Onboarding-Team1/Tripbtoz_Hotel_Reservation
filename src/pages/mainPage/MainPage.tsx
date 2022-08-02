2f1cba46150772efc3ef57de8e6689a50dfcd678
Marco <jonghyeok.lim14@gmail.com>
2022-08-02 19:34:08 +0900

    #8 design:hotellist 퍼블리싱

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -5,11 +5,8 @@ import HotelList from './components/HotelList';
 export default function MainPage() {
   return (
     <div>
-<<<<<<< HEAD
-      <HotelList />
-=======
       <MyCalender />
->>>>>>> e073d1e58c58660406f5b06bcf7283dac0ef55f9
+      <HotelList />
     </div>
   );
 }
--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailPage from 'pages/detailPage/DetailPage';
 import MainPage from 'pages/mainPage/MainPage';
 import StatusPage from 'pages/statusPage/StatusPage';
-<<<<<<< HEAD
-import Hyeok from 'calender/JonghyeokCalender/Hyeok';
-
-=======
 import CalendarLayout from 'calender/ChihangCalender/CalendarLayout';
->>>>>>> e073d1e58c58660406f5b06bcf7283dac0ef55f9
 import Path from './Path';
 
 function Routing() {
@@ -17,12 +12,8 @@ function Routing() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path={Path.status} element={<StatusPage />} />
-<<<<<<< HEAD
         <Route path={Path.detail} element={<DetailPage />} />
-        <Route path={Path.Hyeok} element={<Hyeok />} />
-=======
         <Route path={Path.detail} element={<CalendarLayout />} />
->>>>>>> e073d1e58c58660406f5b06bcf7283dac0ef55f9
       </Routes>
     </BrowserRouter>
   );
