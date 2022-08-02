ba0a66dc29918786f17d8f50c8fe98a2ad056857
chaengs <cathyshim222@naver.com>
2022-08-02 20:20:03 +0900

    design : 호텔 예약 페이지 마크업, CSS

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,10 +1,5 @@
-import MyCalender from 'calender/JihoCalender/MyCalender';
 import React from 'react';
 
 export default function MainPage() {
-  return (
-    <div>
-      <MyCalender />
-    </div>
-  );
+  return <div>main</div>;
 }
