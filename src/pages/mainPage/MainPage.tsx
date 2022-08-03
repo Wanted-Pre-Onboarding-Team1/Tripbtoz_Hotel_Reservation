e2380899e1de78067022c2bfddfc13c02bb65266
Marco <jonghyeok.lim14@gmail.com>
2022-08-03 16:27:21 +0900

    #19 feature:hotellist 무한스크롤

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,13 +1,99 @@
-import React from 'react';
+import { HttpRequest } from 'lib/api/httpRequest';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import HotelList from './components/HotelList';
+import useIntersectObserver from './hooks/useIntersertObserver';
+
+const HOTEL_PAGE = 10;
 
 function MainPage() {
-  const [hotelList, setHotelList] = React.useState();
+  const [hotelList, setHotelList] = React.useState<any>();
+  // 호텔리스트 저장
+  const { isTargetVisible, observerRef } = useIntersectObserver();
+  // 무한 스크롤 훅
+  const [isInitialLoading, setIsInitialLoading] = React.useState<boolean>(true);
+  // 가져올 데이터가 있을 때와 없을 때
+  const [isLoading, setIsLoading] = React.useState(false);
+  // 로딩상태
+  const hotelRequest = new HttpRequest();
+  // API 요청
+
+  const getCurrentPageNumber = (currentHotelList: any) => {
+    const pageNumber = (currentHotelList?.length as number) / HOTEL_PAGE;
+    return Number.isInteger(pageNumber) ? pageNumber : Math.ceil(pageNumber);
+  };
+
+  const currentPage = getCurrentPageNumber(hotelList);
+
+  useEffect(() => {
+    const callback = ({ data }: any) => {
+      setHotelList(data);
+      setIsInitialLoading(false);
+    };
+    hotelRequest.getWithParams({
+      url: 'hotel_list',
+      config: {
+        _page: 1,
+        _limit: HOTEL_PAGE,
+      },
+      callback,
+    });
+  }, []);
+
+  useEffect(() => {
+    const callback = ({ data }: any) => {
+      setHotelList((prevMovieList: any) => [...prevMovieList, ...data]);
+    };
+    setIsLoading(true);
+    isTargetVisible &&
+      !isInitialLoading &&
+      hotelRequest.getWithParams({
+        url: 'hotel_list',
+        config: {
+          _page: currentPage + 1,
+          _limit: HOTEL_PAGE,
+        },
+        callback,
+      });
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, [isTargetVisible, isInitialLoading, currentPage]);
+
   return (
-    <div>
-      <HotelList />
-    </div>
+    <StyledArticle>
+      <StyledText>
+        <StyledPoint>1000</StyledPoint>개의 호텔 중 예약가능 호텔
+        <StyledPoint>{hotelList?.length}</StyledPoint>개
+      </StyledText>
+      <div>
+        {hotelList?.map((value: any, index: number) => (
+          <HotelList key={index} value={value} />
+        ))}
+      </div>
+      {isLoading && <p>loading...</p>}
+      <div style={{ height: '10px' }} ref={observerRef} />
+    </StyledArticle>
   );
 }
 
 export default MainPage;
+
+const StyledArticle = styled.div`
+  width: 100%;
+`;
+
+const StyledText = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  border: 1px solid #f3f3f3;
+  border-radius: 10px;
+  padding: 40px 20px;
+  box-shadow: 2px 3px 5px 0 #f3f3f3;
+  font-size: 1.2rem;
+`;
+
+const StyledPoint = styled.span`
+  font-weight: 500;
+`;
--- src/pages/mainPage/components/HotelList.tsx
+++ src/pages/mainPage/components/HotelList.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function HotelList() {
+function HotelList({ value }: any) {
   return (
     <StyledList>
-      <StyledText>
-        <StyledPoint>1,691</StyledPoint>개의 호텔 중 예약가능 호텔
-        <StyledPoint>316</StyledPoint>개
-      </StyledText>
       <StyledArticle>
         <Styledimg
           src="https://source.unsplash.com/collection/3989638/"
@@ -15,11 +11,11 @@ function HotelList() {
         />
         <StyledTitle>
           <StyledGrade>5.0성급</StyledGrade>
-          <StyledName>웨스틴 조선 서울</StyledName>
-          <StyledOccupancy>기준 2인 | 최대 4인</StyledOccupancy>
+          <StyledName>{value.hotel_name}</StyledName>
+          <StyledOccupancy>{value.occupancy.base}</StyledOccupancy>
         </StyledTitle>
         <StyledPrice>
-          407,000원<StyledTex>세금 및 수수료 불포함</StyledTex>
+          100,000원<StyledTex>세금 및 수수료 불포함</StyledTex>
         </StyledPrice>
       </StyledArticle>
     </StyledList>
@@ -30,25 +26,10 @@ export default HotelList;
 
 const StyledList = styled.section`
   width: 60%;
-  height: 100vh;
   margin: 0 auto;
   @media screen and (max-width: 480px) {
     width: 90%;
   }
-`;
-
-const StyledText = styled.div`
-  width: 100%;
-  margin-bottom: 20px;
-  border: 1px solid #f3f3f3;
-  border-radius: 10px;
-  padding: 40px 20px;
-  box-shadow: 2px 3px 5px 0 #f3f3f3;
-  font-size: 1.2rem;
-`;
-
-const StyledPoint = styled.span`
-  font-weight: 500;
 `;
 
 const StyledArticle = styled.article`
@@ -58,13 +39,16 @@ const StyledArticle = styled.article`
   margin-bottom: 10px;
   border: 1px solid #f3f3f3;
   border-radius: 10px;
+  transition: all 0.5s;
   :hover {
-    box-shadow: 2px 3px 5px 3px #f3f3f3;
+    box-shadow: 2px 3px 5px 5px #f3f3f3;
   }
+`;
+
+const StyledTitle = styled.div`
+  flex: 1;
+  margin: 20px 0 0 20px;
   @media screen and (max-width: 480px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 `;
 
@@ -77,13 +61,6 @@ const Styledimg = styled.img`
   }
 `;
 
-const StyledTitle = styled.div`
-  flex: 1;
-  margin: 20px 0 0 20px;
-  @media screen and (max-width: 480px) {
-  }
-`;
-
 const StyledGrade = styled.div`
   width: max-content;
   margin-bottom: 10px;
@@ -92,11 +69,17 @@ const StyledGrade = styled.div`
   padding: 4px 7px;
   font-size: 1rem;
   box-shadow: 0 1px 1px 0;
+  @media screen and (max-width: 480px) {
+    font-size: 0.8rem;
+  }
 `;
 const StyledName = styled.div`
   margin-bottom: 10px;
   font-size: 1.5rem;
   font-weight: 600;
+  @media screen and (max-width: 480px) {
+    font-size: 1.2rem;
+  }
 `;
 const StyledOccupancy = styled.div`
   color: gray;
@@ -111,10 +94,16 @@ const StyledPrice = styled.div`
   font-weight: 500;
   font-size: 1.5rem;
   align-items: flex-end;
+  @media screen and (max-width: 480px) {
+    font-size: 1.2rem;
+  }
 `;
 
 const StyledTex = styled.div`
   margin-top: 10px;
   font-size: 0.7rem;
   color: gray;
+  @media screen and (max-width: 480px) {
+    font-size: 0.4rem;
+  }
 `;
