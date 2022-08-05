3ff71c3a0cf851b1e97dd0e5f6e933bfc6258668
HyeonJu <hhjc017@gmail.com>
2022-08-06 02:29:54 +0900

    feat : 숙박 예약 날짜에 따라 예약 가능한 호텔 목록 받아오기

--- src/pages/mainPage/utils/getStorageHotels.ts
+++ src/pages/mainPage/utils/getStorageHotels.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-plusplus */
 
-import { isAfter, isBefore, isEqual } from 'date-fns';
+import { isAfter, isBefore, isEqual, parseJSON } from 'date-fns';
 
 /* eslint-disable prettier/prettier */
 export function getReservedHotels() {
@@ -25,8 +25,22 @@ export function getAvailableHotels(hotelList: any[], date: { checkin: Date, chec
     let availableHotels = hotelList ? [...hotelList] : [];
 
     for (let i = 0; i < reservedHotelNames.length; i++) {
-        availableHotels =
-            availableHotels?.filter((hotel) => (hotel.hotel_name !== reservedHotelNames[i])) as any[];
+        const reserveDate = reservedHotels[i][reservedHotelNames[i]][0].date;
+        const reservedHotel = reservedHotelNames[i];
+
+        availableHotels = availableHotels?.filter((hotel) => {
+            if (hotel.hotel_name !== reservedHotel) return true;
+            return (
+                // 사용자가 입력한 date와 local에 저장된 date 비교해서, 날짜가 겹치지 않았을 때에만 예약 가능 호텔 목록에 추가
+                !(isAfter(date.checkin, parseJSON(reserveDate.checkin))
+                    && isBefore(date.checkin, parseJSON(reserveDate.checkout))) &&
+                !(isAfter(date.checkout, parseJSON(reserveDate.checkin))
+                    && isBefore(date.checkout, parseJSON(reserveDate.checkout)))
+                && !isEqual(date.checkin, parseJSON(reserveDate.checkin))
+                && !isEqual(date.checkout, parseJSON(reserveDate.checkout))
+            )
+
+        })
     }
     return availableHotels;
 }
